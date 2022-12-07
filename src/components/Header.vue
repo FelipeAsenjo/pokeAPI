456a662330c108e126c3e9fc889153f9bab01439
Felipe Asenjo <felipe.asenjo2@gmail.com>
2022-12-06 21:35:28 -0300

    responsive pokemonList view & styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
+  <!-- <section> -->
     <img
       class="w-3/4 lg:w-1/3 my-10"
       alt="Vue logo"
       src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
     >
+  <!-- </section> -->
 </template>
 
 <script>
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,11 +1,13 @@
 <template>
-    <ul>
-        <li v-for="pokemon in pokemons" :key="pokemon.id" @click="fetchPokemonDetails(pokemon.id)">
-            <img :src="`${imgUrl}${pokemon.id}.png`" class="w-24"/>
-            {{ pokemon.id}}
-            {{ pokemon.name }}
-        </li>
-    </ul>
+    <section class="my-5 w-5/6 max-w-2xl flex flex-row flex-wrap justify-center">
+        <div 
+            class="my-2 mx-auto w-40 h-40 text-center flex flex-col justify-center bg-gray-200 rounded-lg shadow-md md:mx-2"
+            v-for="pokemon in pokemons" :key="pokemon.id" @click="showPokemon(pokemon.id)"
+        >
+                <img :src="`${imgUrl}${pokemon.id}.png`" class="w-24 mx-auto" />
+                {{ pokemon.name[0].toUpperCase() + pokemon.name.substring(1) }}
+        </div>
+    </section>
 </template>
 
 <script>
@@ -21,7 +23,7 @@ export default {
         pokemons: Object
     },
     methods: {
-        fetchPokemonDetails(id) {
+        showPokemon(id) {
             this.$store.dispatch('fetchPokeDetails', id)
         }
     },
